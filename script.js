e053616d7652c03b58eadc8aea5124c87e54b294
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-04 14:52:34 -0300

    Requisito 2 finalizado, paleta de cores adicionada

---
+++ script.js
@@ -0,0 +1,6 @@
+const colorPalette = document.getElementsByClassName('color');
+
+colorPalette[0].style.backgroundColor = 'black';
+colorPalette[1].style.backgroundColor = 'red';
+colorPalette[2].style.backgroundColor = 'green';
+colorPalette[3].style.backgroundColor = 'blue';
