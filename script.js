498ef5e46e5fd7e74b7283c7bf08aef982373f47
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-04 17:34:43 -0300

    Requisito 5 finalizado

--- script.js
+++ script.js
@@ -6,15 +6,31 @@ colorPalette[2].style.backgroundColor = 'green';
 colorPalette[3].style.backgroundColor = 'blue';
 
 const randomColor = document.getElementById('button-random-color');
-
+const localObj = {};
 randomColor.addEventListener('click', () => {
   for (let i = 1; i < colorPalette.length; i += 1) {
     const rndNumber1 = Math.random() * (255 - 0) + 0;
     const rndNumber2 = Math.random() * (255 - 0) + 0;
     const rndNumber3 = Math.random() * (255 - 0) + 0;
     colorPalette[i].style.backgroundColor = `rgb(${rndNumber1},${rndNumber2},${rndNumber3})`;
-    localStorage.setItem(`color${i}`, colorPalette[i].style.backgroundColor);
+    const keyObj = `color${i}`;
+    localObj[keyObj] = colorPalette[i].style.backgroundColor;
+    localStorage.setItem('colorPalette', JSON.stringify(localObj));
   }
 });
 
+const pixel = document.getElementById('parent-pixel');
+for (let i = 1; i <= 25; i += 1) {
+  const pixelDiv = document.createElement('div');
+  pixelDiv.className = 'pixel';
+  pixel.appendChild(pixelDiv);
+}
 
+const storageColor = () => {
+  const colorParse = localStorage.getItem('colorPalette');
+  colorPalette[1].style.backgroundColor = JSON.parse(colorParse).color1;
+  colorPalette[2].style.backgroundColor = JSON.parse(colorParse).color2;
+  colorPalette[3].style.backgroundColor = JSON.parse(colorParse).color3;
+};
+
+window.onload = storageColor;
